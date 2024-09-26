42ac0956a7503fcf4ca9b3a37c662f9d8017d3f1
dannielsss <gamekik92@gmail.com>
2024-09-26 17:36:44 -0600

    add morgan

--- src/apps/api/src/app.ts
+++ src/apps/api/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 
 import taskRoutes from './Context/task/infraestructure/http/TaskRoutes';
 import labelRoutes from './Context/label/infraestructure/http/LabelRoutes';
@@ -6,6 +7,7 @@ import labelRoutes from './Context/label/infraestructure/http/LabelRoutes';
 const app = express();
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.use('/api/tasks', taskRoutes);
 app.use('/api/labels', labelRoutes);
